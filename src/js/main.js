5cab57b04467a09510a7da706a465f32af4c7e9c
yoanbonjour <yoan.bonjour@studentfr.ch>
2026-03-23 11:25:19 +0100

    Hover de l'image

--- src/js/main.js
+++ src/js/main.js
@@ -19,18 +19,69 @@ var loadPromise = new Promise((resolve) => {
 });
 
 var timerPromise = new Promise((resolve) => {
-  setTimeout(resolve, 6000);
+  setTimeout(resolve, 1000);
 });
 
 // When both load and timer are done, transition
 Promise.all([loadPromise, timerPromise]).then(() => {
   gsap.to(loadingPage, {
     opacity: 0,
-    duration: 2,
+    duration: 0.5,
     onComplete: () => {
       loadingPage.style.display = "none";
       content.style.display = "block";
-      gsap.to(content, { opacity: 1, duration: 2 });
+      gsap.to(content, { opacity: 1, duration: 0.5 });
     },
   });
 });
+
+// Loupe effect
+const zoom = 1;
+const loupe = document.getElementById("loupe");
+const landscapeImg = document.querySelector(".image-container img:first-child");
+const landscape2Img = document.getElementById("landscape-2");
+
+landscapeImg.addEventListener("click", function () {
+  console.log("Loupe clicked");
+  gsap.to("#loupe", {
+    width: 2000,
+    height: 2000,
+    duration: 1,
+    ease: "easeInOut",
+  });
+});
+
+if (landscapeImg && landscape2Img) {
+  landscapeImg.addEventListener("mousemove", function (event) {
+    const rect = landscapeImg.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    loupe.style.display = "block";
+    loupe.style.left = event.pageX - 100 + "px";
+    loupe.style.top = event.pageY - 100 + "px";
+    loupe.style.backgroundSize = rect.width * zoom + "px auto";
+    loupe.style.backgroundPosition =
+      -x * zoom + 100 + "px " + (-y * zoom + 100) + "px";
+  });
+
+  landscape2Img.addEventListener("mousemove", function (event) {
+    const rect = landscape2Img.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    loupe.style.display = "block";
+    loupe.style.left = event.pageX - 100 + "px";
+    loupe.style.top = event.pageY - 100 + "px";
+    loupe.style.backgroundSize = rect.width * zoom + "px auto";
+    loupe.style.backgroundPosition =
+      -x * zoom + 100 + "px " + (-y * zoom + 100) + "px";
+  });
+
+  const hideLoupe = () => {
+    loupe.style.display = "none";
+  };
+
+  landscapeImg.addEventListener("mouseleave", hideLoupe);
+  landscape2Img.addEventListener("mouseleave", hideLoupe);
+}
